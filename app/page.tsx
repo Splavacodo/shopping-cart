7e1ad63d6dad1d1f0808e4023de773e7fd4df535
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-20 23:27:45 -0700

    style: Simplified root page layout by modifying the root layout to be a flex container

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full flex-col justify-center items-center py-32 px-16">
-        <div>yo</div>
-      </main>
-    </div>
+    <main className="flex-1 flex flex-col font-sans items-center justify-center">
+      <div>yo</div>
+    </main>
   );
 }
