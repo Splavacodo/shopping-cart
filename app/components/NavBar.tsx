65ccafcea922d9175e5bb6f45329280540e01afe
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-20 23:29:32 -0700

    feat: created ShopNavLink component that links to a /shop route and removed padding from NavBar component styling

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,13 +2,14 @@
 
 import { useCart } from "../contexts/CartContext";
 import NavStoreHeader from "./NavStoreHeader";
+import ShopNavLink from "./ShopNavLink";
 
 export default function NavBar() {
     const { cart } = useCart();
 
     return (
-        <nav className="flex justify-around items-center py-6 border-b-zinc-700 border-b-1 font-sans">
-            <div>Shop</div>
+        <nav className="flex justify-around items-center border-b-zinc-700 border-b-1 font-sans">
+            <ShopNavLink/>
             <NavStoreHeader/>
             <div>{`Cart (${cart.length})`}</div>
         </nav>
---
+++ app/shop/page.tsx
@@ -0,0 +1,9 @@
+export default function Shop() {
+    return (
+        <main className="flex-1 flex flex-col font-sans items-center justify-center">
+            <section>
+                <div>Shop Products</div>
+            </section>
+        </main>
+    );
+}
