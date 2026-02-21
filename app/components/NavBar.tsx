c3393ea609208920f951c78ad593324f6bda69d5
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-20 21:22:47 -0700

    feat: Replaced temporary shop title nav div with a NavStoreHeader component with some basic initial styling

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { useCart } from "../contexts/CartContext";
+import NavStoreHeader from "./NavStoreHeader";
 
 export default function NavBar() {
     const { cart } = useCart();
 
     return (
-        <nav className="flex justify-around items-center">
+        <nav className="flex justify-around items-center py-6 border-b-zinc-700 border-b-1 font-sans">
             <div>Shop</div>
-            <div>Fake Store</div>
+            <NavStoreHeader/>
             <div>{`Cart (${cart.length})`}</div>
         </nav>
     )
---
+++ app/components/NavStoreHeader.tsx
@@ -0,0 +1,7 @@
+export default function NavStoreHeader() { 
+    return (
+        <h1 className="font-semibold text-xl text-foreground">
+            FakeStore
+        </h1>
+    )
+}
