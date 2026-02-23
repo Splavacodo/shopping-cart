c5b92655dfc1984ace0efb571aa7e562db875afc
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-22 21:19:47 -0700

    feat: modified the NavStoreHeader component to be a link that routes back to home page and removed redudant cursor styling on ShopNavLink component

--- app/components/NavStoreHeader.tsx
+++ app/components/NavStoreHeader.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function NavStoreHeader() { 
     return (
-        <h1 className="font-semibold text-xl text-foreground">
-            FakeStore
-        </h1>
+        <Link href="/">
+            <h1 className="font-semibold text-xl text-foreground">
+                FakeStore
+            </h1>
+        </Link>
     )
 }
--- app/components/ShopNavLink.tsx
+++ app/components/ShopNavLink.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ShopNavLink() {
     return (
-        <Link className="border-b-2 border-b-transparent py-6 hover:cursor-pointer hover:border-b-foreground hover:border-b-2" href="/shop">
+        <Link className="border-b-2 border-b-transparent py-6 hover:border-b-foreground hover:border-b-2" href="/shop">
             Shop
         </Link>
     );
