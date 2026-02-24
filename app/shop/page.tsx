778e7c34da0eaa7498760e5bef62d039ea976b44
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-23 17:32:55 -0700

    feat: Replaced temp /shop page content with a starting shop products header with initial basic styling

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,8 +1,10 @@
 export default function Shop() {
     return (
-        <main className="flex-1 flex flex-col font-sans items-center justify-center">
-            <section>
-                <div>Shop Products</div>
+        <main className="flex-1 flex flex-col items-center font-sans py-8">
+            <section className="flex flex-col w-full max-w-8/10">
+                <header>
+                    <h1 className="font-semibold text-xl">Shop Products</h1>
+                </header>
             </section>
         </main>
     );
