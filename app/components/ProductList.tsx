0f1d5382add43abd979f0eaae87d9323d58be621
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-27 22:45:59 -0700

    feat: Updated ProductList component to render a list of products instead of a single product now that adding to a cart is working

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,18 +1,21 @@
 import ProductCard from "./ProductCard";
 
 export default async function ProductList() {
-    const products = await fetch("https://fakestoreapi.com/products")
+    const products: Array<Record<string, string>> = await fetch("https://fakestoreapi.com/products")
         .then((response) => response.json())
         .then((data) => data);
 
     return (
         <div className="grid gap-8 [grid-template-columns:repeat(auto-fill,minmax(300px,1fr))]">
-            <ProductCard
-                productId={products[0].id}
-                productImgURL={products[0].image}
-                productTitle={products[0].title}
-                productPrice={products[0].price}
-            />
+            {products.map(product => 
+                <ProductCard
+                    key={product.id}
+                    productId={product.id}
+                    productImgURL={product.image}
+                    productTitle={product.title}
+                    productPrice={product.price}
+                />
+            )}
         </div>
     )
 }
