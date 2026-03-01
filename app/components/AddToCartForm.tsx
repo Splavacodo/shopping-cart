895814afacf6b7d6c3b7a5a509fc5ba5939f5db2
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-28 17:46:18 -0700

    fix: Fixed add to cart form validation by moving the event listener from the add to cart button to the form itself

--- app/components/AddToCartForm.tsx
+++ app/components/AddToCartForm.tsx
@@ -5,7 +5,7 @@ import { useCart } from "../contexts/CartContext";
 export default function AddToCartForm({ productId, productTitle, productPrice }: { productId: string, productTitle: string, productPrice: number}) {
     const { addItem } = useCart();
 
-    function handleAddToCart(event: React.MouseEvent<HTMLButtonElement>) {
+    function handleAddToCart(event: React.SubmitEvent<HTMLFormElement>) {
         event.preventDefault();
 
         const form = event.currentTarget.closest("form");
@@ -23,10 +23,10 @@ export default function AddToCartForm({ productId, productTitle, productPrice }:
     }
 
     return (
-        <form className="flex justify-center items-center gap-4">
+        <form className="flex justify-center items-center gap-4" onSubmit={handleAddToCart}>
             <label htmlFor="prod-qty" className="text-sm">Qty:</label>
-            <input id="prod-qty" name="prod-qty" min={1} max={5} type="number" placeholder="1" className="rounded-sm text-sm border border-zinc-700 p-1"></input>
-            <button onClick={handleAddToCart} className="rounded-sm bg-foreground text-background py-2 px-4 hover:cursor-pointer">Add to cart</button>
+            <input id="prod-qty" name="prod-qty" min={1} max={5} type="number" placeholder="1" className="rounded-sm text-sm border border-zinc-700 p-1" required></input>
+            <button type="submit" className="rounded-sm bg-foreground text-background py-2 px-4 hover:cursor-pointer">Add to cart</button>
         </form>
     );
 }
